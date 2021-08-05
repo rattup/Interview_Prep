fd9a77b8e3a9a1ce70f2b21e97e557efe73edb75
rattup <42693033+rattup@users.noreply.github.com>
2021-08-05 09:47:03 +0530

    Add files via upload

---
+++ delete_node_in_a_BST.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int predecessor(TreeNode* root)
+    {
+        root=root->left;
+        while(root->right)
+        root=root->right;
+        return root->val;
+    }
+     int successor(TreeNode* root)
+    {
+        root=root->right;
+        while(root->left)
+        root=root->left;
+        return root->val;
+    }
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(!root)
+        return root;
+        if(key>root->val)
+        root->right=deleteNode(root->right,key);
+        else if(key<root->val)
+        root->left=deleteNode(root->left,key);
+        else
+        {
+         if(!root->left && !root->right)
+            root=nullptr;
+        else if(root->left)
+        {
+         root->val=predecessor(root);
+        root->left=deleteNode(root->left,root->val);
+        
+        }
+        else
+        {
+            root->val=successor(root);
+            root->right=deleteNode(root->right,root->val);
+            
+        }
+        
+        }
+        return root;
+        
+    }
+};
