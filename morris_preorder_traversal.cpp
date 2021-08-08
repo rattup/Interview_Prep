82316a21ab2a4bbf36854d2cb73a705a79e4990a
rattup <42693033+rattup@users.noreply.github.com>
2021-08-08 15:48:55 +0530

    Add files via upload

---
+++ morris_preorder_traversal.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* predecessor(TreeNode* root,TreeNode* cur)
+    {
+        while(root->right && root->right!=cur)
+        root=root->right;
+        return root;
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        TreeNode* cur;
+        cur=root;
+        while(cur)
+        {
+            if(!cur->left)
+            {
+                res.push_back(cur->val);
+                cur=cur->right;
+            }
+            else
+            {
+                TreeNode* pre=predecessor(cur->left,cur);
+                if(!pre->right)
+                {
+                	
+                    pre->right=cur;
+                    res.push_back(cur->val);
+                    cur=cur->left;
+                }
+                else
+                {
+                    pre->right=nullptr;
+                    cur=cur->right;
+                }
+            }
+        }
+        return res;
+        
+    }
+};
